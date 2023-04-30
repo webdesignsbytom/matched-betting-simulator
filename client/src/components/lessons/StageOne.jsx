0ee3e4ad030be422d9103d98d1d17a448a8e8d16
Thomas Brockington <xtombrock1989@gmail.com>
2023-04-30 23:33:25 +0100

    save

--- client/src/components/lessons/StageOne.jsx
+++ client/src/components/lessons/StageOne.jsx
@@ -3,43 +3,48 @@ import { Link } from "react-router-dom";
 
 function StageOne() {
   return (
-    <article className="p-2">
-      <div className="grid grid-flow-col gap-4 w-fit mb-2">
-        <h3 className="font-semibold">Stage One: </h3>
-        <h4>Find A Free Bet Offer</h4>
-      </div>
+    <section>
+      <article className="p-2 pb-10 border-b-2 border-yellow-400 border-solid">
+        <div className="grid grid-flow-col gap-4 w-fit mb-2">
+          <h3 className="font-semibold">Stage One: </h3>
+          <h4>Find A Free Bet Offer</h4>
+        </div>
 
-      <div className="outline-2 outline-black outline p-1 mb-2">
-        <p>
-          Find a bookie with a free Bet offer, if this is your first time and
-          keep it simple with a 'Stake Not Returned' free bet. You can find a
-          good bet you can trust by following one of our{" "}
-          <span>
-            <Link to="/links">
-              <span className="text-yellow-400 font-bold">Links</span>
-            </Link>
-          </span>
-        </p>
-      </div>
+        <div className="outline-2 outline-black outline p-1 mb-2">
+          <p>
+            Find a bookie with a free Bet offer, if this is your first time and
+            keep it simple with a 'Stake Not Returned' free bet. You can find a
+            good bet you can trust by following one of our{" "}
+            <span>
+              <Link to="/links">
+                <span className="text-yellow-400 font-bold">Links</span>
+              </Link>
+            </span>
+          </p>
+        </div>
 
-      <section className="">
-        <article className="">
-          <div>
-            <h6 className='font-semibold'>Example Data For A Bet</h6>
-          </div>
-          <div className='grid md:grid-cols-3'>
-            <p>Company: Ladbrokes</p>
-            <p>End Date: ? When does the offer End ?</p>
-            <p>BetType: SNR/SR/REFUND </p>
-            <p>Qualifying Bet: £10</p>
-            <p>Minimum Bet: £10</p>
-            <p>Potential Profit: £7.70</p>
-            <p>Link: https:www.ladbrokes.com </p>
-            <p>Description: Sign up, dpotis £5 and get £20 worth of free bets. SNR type bets</p>
-          </div>
-        </article>
-      </section>
-    </article>
+        <section className="">
+          <article className="">
+            <div>
+              <h6 className="font-semibold">Example Data For A Bet</h6>
+            </div>
+            <div className="grid md:grid-cols-3">
+              <p>Company: Ladbrokes</p>
+              <p>End Date: ? When does the offer End ?</p>
+              <p>BetType: SNR/SR/REFUND </p>
+              <p>Qualifying Bet: £10</p>
+              <p>Minimum Bet: £10</p>
+              <p>Potential Profit: £7.70</p>
+              <p>Link: https:www.ladbrokes.com </p>
+              <p>
+                Description: Sign up, dpotis £5 and get £20 worth of free bets.
+                SNR type bets
+              </p>
+            </div>
+          </article>
+        </section>
+      </article>
+    </section>
   );
 }
 
--- client/src/components/lessons/StageTwo.jsx
+++ client/src/components/lessons/StageTwo.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { GameContext } from '../../context/GameContext';
+import React, { useContext } from "react";
+import { GameContext } from "../../context/GameContext";
 
 function StageTwo() {
   const {
@@ -19,52 +19,94 @@ function StageTwo() {
   } = useContext(GameContext);
 
   const handleBookieChange = (event) => {
-    console.log('handleChange', event.target.value);
-    console.log('handleChange', event.target.name);
+    console.log("handleChange", event.target.value);
+    console.log("handleChange", event.target.name);
     const { name, value } = event.target;
 
     setBookieDepositData(Number(value));
   };
-  console.log('bookieDepositData', bookieDepositData);
+  console.log("bookieDepositData", bookieDepositData);
 
   const submitBookieDeposit = (event) => {
     event.preventDefault();
-    console.log('submit');
+    console.log("submit");
 
     if (bookieDepositData > playerBank) {
-      return alert('Sorry not enough funds to continue')
+      return alert("Sorry not enough funds to continue");
     }
     setPlayerBank((current) => current - bookieDepositData);
     setBookieBetzBank((current) => current + bookieDepositData);
-
   };
 
-
   return (
-    <article className='stage__container'>
-      <div className='stage__title__container'>
-        <h3>Stage Two: </h3>
-        <h4>Join the bookie</h4>
-      </div>
+    <section>
+      <article className="p-2 pb-10 border-b-2 border-yellow-400 border-solid">
+        <div className="grid grid-flow-col gap-4 w-fit mb-2">
+          <h3 className="font-semibold">Stage One: </h3>
+          <h4>Join the bookie</h4>
+        </div>
 
-      <div className='stage__info__container'>
-        <p>
-          So you have found a free bet offer. BookieBetz are offering bet £10
-          and get a £10 free bet. Sign up being sure to check the rules, terms,
-          countries, and withdrawal limits/minimums. Check any boxes required.
-          Lets deposit £10 and move on the next step for now.{' '}
-        </p>
-      </div>
+        <div className="my-2">
+          <p>
+            So you have found a free bet offer. BookieBetz are offering bet £10
+            and get a £10 free bet. Sign up being sure to check the rules,
+            terms, countries, and withdrawal limits/minimums. Check any boxes
+            required. Lets deposit £10 and move on the next step for now.{" "}
+          </p>
+          <p>
+            Our examples will be based on{" "}
+            <a
+              href="https:www.williamhill.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>williamhill.com</span>
+            </a>{" "}
+            which always has a decent offer and easy to understand terms and
+            conditions.
+          </p>
+        </div>
 
-      <section className='stage__example__container'>
-        <input
-          type='number'
-          name='betDataDeposit'
-          onChange={handleBookieChange}
-        />
-        <button onClick={submitBookieDeposit}>DEPOSIT</button>
-      </section>
-    </article>
+        <section className='grid md:grid-cols-2'>
+          <article>
+            <h6>OFFER</h6>
+            <p>Welcome to William Hill</p>
+            <p>Sign Up and deposit and bet £10 to get £30 in free bets!</p>
+            <p>Free bets come in 3 times £10 bets</p>
+          </article>
+          <article>
+            <h6>WHAT TO NOTE</h6>
+            <p>We must deposit and bet £10 from our bank and bet it.</p>
+            <p>Its always best to stick to football and horse racing.</p>
+            <p>After we have bet £10 (and the game has finished and been called) you will get 3x £10 free bets. That can be found on your bet slip when you come to place a bet.</p>
+            <p>Always check the T&C's for a min and max bet on your free and qaulifying bet.</p>
+            <p>Always find your odds in decimal form</p>
+            <p>Observe how we track your funds across all accounts</p>
+          </article>
+        </section>
+
+        <section className="grid">
+          <div className="grid grid-flow-col w-fit">
+            <input
+              type="number"
+              name="bookieDeposit"
+              id="bookieDeposit"
+              className="max-w-[150px p-1"
+              onChange={handleBookieChange}
+              placeholder='£10'
+            />
+            <div>
+              <button
+                className="outline-black outline outline-2 bg-orange-400 p-1"
+                onClick={submitBookieDeposit}
+              >
+                DEPOSIT
+              </button>
+            </div>
+          </div>
+        </section>
+      </article>
+    </section>
   );
 }
 
